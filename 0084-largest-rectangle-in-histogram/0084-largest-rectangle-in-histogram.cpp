518ad8744766f0647650657392ee8cc6af00e48d
karthikcsit <112811909+karthikcsit@users.noreply.github.com>
2023-03-19 14:59:19 +0530

    Time: 283 ms (13.77%), Space: 92.3 MB (20.16%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,69 +1,74 @@
 class Solution {
 public:
    
-vector<int> nsl(vector<int>& heights, vector<int>&left,int n)
-{
-      
-        int pseudopos=-1;
-       
-        stack<pair<int,int>>s;
-        for(int i=0;i<n;i++)
+int largestRectangleArea(vector<int>& heights) {
+        int n= heights.size();
+        vector<int> left,right;
+    stack<pair<int,int>> s1,s2;
+    int pseudo_index =-1;
+    int pseudo_index1 =n;
+    for (int i=0;i<n;i++)
+    {
+      if (s1.size()==0)
+      {
+        left.push_back(pseudo_index);
+      }
+      else if (s1.size()>0 && s1.top().first<heights[i])
+      {
+        left.push_back(s1.top().second);
+      }
+      else if (s1.size()>0 && s1.top().first>=heights[i])
+      {
+         while(s1.size()>0 && s1.top().first>=heights[i])
+         {
+           s1.pop();
+         }
+        if (s1.size()==0)
         {
-            if(s.size()==0) 
-                left.push_back(pseudopos);
-            else if(s.size()>0 && s.top().first<heights[i])
-                left.push_back( s.top().second);
-            else if(s.size()>0 && s.top().first>=heights[i])
-            {
-                while(s.size()>0 && s.top().first>=heights[i])
-                    s.pop();
-                if(s.size()==0) left.push_back(pseudopos);
-                else
-                   left.push_back( s.top().second); 
-            }
-            s.push({heights[i],i});
+          left.push_back(pseudo_index);
         }
-    return left;
-}       
- vector<int> nsr(vector<int>& heights,vector<int>&right,int n)  
-  {    
-        int pseudopos=n;
-      
-        stack<pair<int,int>>st;
-         for(int i=n-1;i>=0;i--)
+        else
         {
-            if(st.size()==0) right.push_back(pseudopos);
-            else if(st.size()>0 && st.top().first<heights[i])
-                right.push_back( st.top().second);
-            else if(st.size()>0 && st.top().first>=heights[i])
-            {
-                while(st.size()>0 && st.top().first>=heights[i])
-                    st.pop();
-                if(st.size()==0) 
-                    right.push_back(pseudopos);
-                else
-                   right.push_back( st.top().second); 
-            }
-            st.push({heights[i],i});
+          left.push_back(s1.top().second);
         }
-        reverse(right.begin(),right.end());
-          return right;
- }
-     int largestRectangleArea(vector<int>& heights) 
-     {
-          int n=heights.size();
-          vector<int>left;
-         vector<int>right;
-         nsl( heights,left,n);
-         nsr( heights,right,n);
-        vector<int>ans;
-        for(int i=0;i<n;i++)
+      }
+      s1.push({heights[i],i});
+    }
+    for (int i=n-1;i>=0;i--)
+    {
+      if (s2.size()==0)
+      {
+        right.push_back(pseudo_index1);
+      }
+      else if (s2.size()>0 && s2.top().first<heights[i])
+      {
+        right.push_back(s2.top().second);
+      }
+      else if (s2.size()>0 && s2.top().first >= heights[i])
+      {
+        while(s2.size()>0 && s2.top().first >= heights[i])
         {
-         int   z=heights[i]* (right[i]-left[i]-1);
-            ans.push_back(z);
+          s2.pop();
         }
-         sort(ans.begin(),ans.end());
-       
-        return ans[heights.size()-1];
-     }
+        if (s2.size()==0)
+        {
+          right.push_back(pseudo_index1);
+        }
+        else
+        {
+          right.push_back(s2.top().second);
+        }
+      }
+      s2.push({heights[i],i});
+    }
+    reverse(right.begin(),right.end());
+    int m=INT_MIN;
+    for (long long i=0;i<n;i++)
+    {
+      m=max(m,(right[i]-left[i]-1)*heights[i]);// taking max after finding area
+    }
+    return m;
+    }
+
+   
 };
