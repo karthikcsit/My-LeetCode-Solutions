29a9e5a0fa91f43bcd4aa3c46bad50e26b6c1c08
karthikcsit <112811909+karthikcsit@users.noreply.github.com>
2023-04-09 23:10:49 +0530

    Time: 3 ms (100.00%), Space: 10.7 MB (93.33%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-   //  int t[1001][1001];
-   /* int lcs(string str1, string str2,int m,int n) {
+   // int t[1001][1001];
+    string lcs(string str1, string str2,int m,int n) {
         
         
-     //   int t[m+1][n+1];
+       int t[m+1][n+1];
          for(int i=0;i<m+1;i++)
             for(int j=0;j<n+1;j++)
                 if(i==0||j==0)
@@ -21,33 +21,8 @@ public:
                     t[i][j]=max(t[i][j-1],t[i-1][j]);
             }
          }
-        return t[m][n];
-                
-
-        
-    }*/
-  /*  string shortestCommonSupersequence(string str1, string str2) {
-        int m=str1.length();
-        int n=str2.length();
-      //  lcs(str1,str2,m,n);
-        int t[m+1][n+1];
-        for(int i=0;i<m+1;i++)
-            for(int j=0;j<n+1;j++)
-                if(i==0||j==0)
-                    t[i][j]=0;
-        
-         for(int i=1;i<m+1;i++)
-         {
-            for(int j=1;j<n+1;j++)
-            {
-                if(str1[i-1]==str2[j-1])
-                    t[i][j]= 1+  t[i-1][j-1];
-                
-                else
-                    t[i][j]=max(t[i][j-1],t[i-1][j]);
-            }
-         }
-        int i=m,j=n;
+         
+          int i=m,j=n;
         string s="";
         while(i>0 && j>0)
         {
@@ -59,15 +34,15 @@ public:
             }
             else
             {
-                if(t[i][j-1]>t[i-1][j])
-                {
-                     s.push_back(str2[j-1]);
-                    j--;
-                }
-                else if(t[i][j-1]<t[i-1][j])
+                if(t[i-1][j]>t[i][j-1])
                 {
                      s.push_back(str1[i-1]);
                     i--;
+                }
+                else 
+                {
+                     s.push_back(str2[j-1]);
+                    j--;
                 }
              }
         }
@@ -83,7 +58,17 @@ public:
         }
         reverse(s.begin(),s.end());
         return s;
-    }*/
+        
+                
+
+        
+    }
+    string shortestCommonSupersequence(string str1, string str2) {
+        int m=str1.length();
+        int n=str2.length();
+      string s1=  lcs(str1,str2,m,n);
+       return s1;
+    }
     
     
     
@@ -105,7 +90,7 @@ public:
     
     
     
-    string LCS(string str1, string str2,int m,int n){
+  /*  string LCS(string str1, string str2,int m,int n){
 string s="";
 int t[m+1][n+1];
 for(int i=0;i<m+1;i++){
@@ -161,5 +146,5 @@ string shortestCommonSupersequence(string str1, string str2) {
     return ans;
     
 
-}
+}*/
 };
