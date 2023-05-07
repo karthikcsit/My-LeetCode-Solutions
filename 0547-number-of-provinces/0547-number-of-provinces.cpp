ddcc0905e3d1c7666d261861372a2338f3807d43
karthikcsit <112811909+karthikcsit@users.noreply.github.com>
2023-05-07 13:12:58 +0530

    Time: 25 ms (63.46%), Space: 14.9 MB (35.41%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -3,12 +3,12 @@ public:
     void dfs(int node, vector<int>adjls[] ,  vector<int>&vis)
     {
        
-        
+        vis[node]=1;
         for(auto it:adjls[node])
         {
            if(!vis[it])
            {
-               vis[it]=1;
+              
                dfs(it,adjls,vis);
            }
         }
