f343308cba82f136bd8f2c70c4bb8083239e70cc
karthikcsit <112811909+karthikcsit@users.noreply.github.com>
2023-05-19 22:59:29 +0530

    Time: 93 ms (57.23%), Space: 32.4 MB (86.60%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    /*int countPaths(int n, vector<vector<int>>& roads) {
+    int countPaths(int n, vector<vector<int>>& roads) {
         vector<pair<int,int>>adj[n];
         for(auto it:roads)
         {
@@ -12,7 +12,7 @@ public:
         priority_queue<pair<long long,long long>,
         vector<pair<long long,long long>>,
         greater<pair<long long,long long>>>pq;
-        vector<long long>dist(n,1e9),ways(n,0);
+        vector<long long>dist(n,1e15),ways(n,0);
         dist[0]=0;
         ways[0]=1;
         pq.push({0,0});
@@ -43,39 +43,6 @@ public:
             }
         }
         return ways[n-1];
-    }*/
-     int countPaths(int n, vector<vector<int>>& r) {
-        int mod=1e9+7;
-        vector<vector<pair<long long,long long>>>adj(n);
-        for(int i=0;i<r.size();i++){
-            adj[r[i][0]].push_back({r[i][1],r[i][2]});
-            adj[r[i][1]].push_back({r[i][0],r[i][2]});
-        }
-        vector<long long>c(n,0);
-        c[0]=1;
-        priority_queue<pair<long long,long long>, vector<pair<long long,long long>>, greater<pair<long long,long long>>> pq;
-        vector<long long int>dist(n,1e15);
-        dist[0]=0;
-        pq.push({0,0});
-        while(pq.empty()==false){
-            auto x=pq.top();
-            long long dis=x.first;
-            long long node=x.second;
-            pq.pop();
-            for(auto it:adj[node]){
-                long long wt=it.second;
-                long long y=it.first; 
-                if(wt+dis<dist[y]){
-                    dist[y]=(wt+dis);
-                    pq.push({dist[y],y});
-                    c[y]=c[node];
-                }
-                else if((wt+dis)==dist[y]){
-                    c[y]=(c[y]+c[node])%mod;
-                }
-                
-            }
-        }
-        return c[n-1];
     }
+    
 };
